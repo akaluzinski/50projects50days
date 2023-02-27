988cd93c79c979844203c36501d6efa84d723686
Adrian Kaluzinski <kaluza@gmail.com>
2023-02-27 21:04:42 +0100

    Pickup random tag

--- 12_random_choice_selector/script.js
+++ 12_random_choice_selector/script.js
@@ -1,6 +1,7 @@
 const tags = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 const separator = ',';
+const activeClassName = 'active';
 
 textarea.focus();
 
@@ -21,8 +22,44 @@ function createTags(value) {
     .forEach((label) => tags.appendChild(createTag(label)));
 }
 
-function onKeyUp({ target }) {
-  createTags(target.value);
+function cleanTextArea(event) {
+  if (event.key === 'Enter') {
+    setTimeout(() => {
+      event.target.value = '';
+    }, 10);
+  }
+}
+
+function markActiveTag(tag) {
+  tag.classList.add(activeClassName);
+}
+
+function markUnactiveTag(tag) {
+  tag.classList.remove(activeClassName);
+}
+
+function pickRandomTag() {
+  const allTags = document.querySelectorAll('.tag');
+  return allTags[Math.floor(Math.random() * allTags.length)];
+}
+
+function selectRandom() {
+  const times = 50;
+
+  const interval = setTimeout(() => {
+    const randomTag = pickRandomTag();
+    console.log(randomTag);
+  }, 100);
+}
+
+function pickUpRandom(event) {
+  cleanTextArea(event);
+  selectRandom();
+}
+
+function onKeyUp(event) {
+  createTags(event.target.value);
+  pickUpRandom(event);
 }
 
 textarea.addEventListener('keyup', onKeyUp);
