8d3542f493fe4b2e2387a401a3243254300b157a
Adrian Kaluzinski <kaluza@gmail.com>
2023-05-08 12:02:43 +0200

    Change slider on right arrow click

---
+++ 18_background_image/script.js
@@ -0,0 +1,30 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+const activeClass = 'active';
+
+let activeSlideIdx = 0;
+rightButton.addEventListener('click', () => {
+  activeSlideIdx++;
+  if (activeSlideIdx > slides.length - 1) {
+    activeSlideIdx = 0;
+  }
+
+  setBackgroundToBody();
+  setActiveSlide();
+});
+
+function setBackgroundToBody() {
+  body.style.backgroundImage = slides[activeSlideIdx].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove(activeClass);
+  });
+
+  slides[activeSlideIdx].classList.add(activeClass);
+}
+
+setBackgroundToBody();
