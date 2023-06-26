764095687ab303af3ad2b9b70d6184cc1ca76875
Adrian Kaluzinski <kaluza@gmail.com>
2023-06-26 13:30:39 +0200

    Rotate all needles

--- 19_clock/script.js
+++ 19_clock/script.js
@@ -1,6 +1,6 @@
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
-const second = document.querySelector('.second');
+const hourNeedle = document.querySelector('.hour');
+const minuteNeedle = document.querySelector('.minute');
+const secondNeedle = document.querySelector('.second');
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
@@ -22,16 +22,19 @@ function setTime() {
     const now = new Date();
     const month = now.getMonth();
     const day = now.getDay();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const hourDegrees = scale(hours % 12, 0, 11, 0, 360)
+    const hourDegrees = rotateDeg(now.getHours() % 12, 11);
+    const minuteDegrees = rotateDeg(now.getMinutes(), 59);
+    const secondsDegrees = rotateDeg(now.getSeconds(), 59);
 
-    hour.style.transform = `translate(-50%, -100%) rotate(${hourDegrees}deg)`;
+    const transformStyle = newDegree => `translate(-50%, -100%) rotate(${newDegree}deg)`
+
+    hourNeedle.style.transform = transformStyle(hourDegrees);
+    minuteNeedle.style.transform = transformStyle(minuteDegrees);
+    secondNeedle.style.transform = transformStyle(secondsDegrees);
 
 }
 
-const scale = (number, inMin, inMax, outMin, outMax) => (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+const rotateDeg = (number, inMax) => number * 360 / inMax
 toggle.addEventListener('click', onDarkModeClick);
 
 setTime();
