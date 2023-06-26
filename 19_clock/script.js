90c132e50e4588226ee17dce2f4937aca622cddd
Adrian Kaluzinski <kaluza@gmail.com>
2023-06-26 13:10:34 +0200

    add selectors

---
+++ 19_clock/script.js
@@ -0,0 +1,7 @@
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
