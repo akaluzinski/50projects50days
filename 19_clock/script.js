20b3ae4933164780b445ca9fc614a1ef28ee0ac2
Adrian Kaluzinski <kaluza@gmail.com>
2023-06-26 13:15:00 +0200

    Toggle dark/light mode on btn click

--- 19_clock/script.js
+++ 19_clock/script.js
@@ -5,3 +5,17 @@ const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
+const darkModeClassName = 'dark';
+
+function onDarkModeClick(event) {
+    const html = document.querySelector('html');
+    if(html.classList.contains(darkModeClassName)) {
+        html.classList.remove(darkModeClassName)
+        event.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add(darkModeClassName);
+        event.target.innerHTML = 'Light mode';
+    }
+}
+
+toggle.addEventListener('click', onDarkModeClick);
